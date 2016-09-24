ce52d686ab7df0088dc361eacb26525efc6fbaa7
Graham St-Laurent <gstlaurent@gmail.com>
2016-09-24 16:35:09 -0700

    Initial commit. Tools for decrypting ciphers.

---
+++ cryptoanalysis.py
@@ -0,0 +1,178 @@
+from collections import Counter, namedtuple, defaultdict
+
+# everything uses lowercase
+
+ALPHAS = "abcdefghijklmnopqrstuvwxyz"
+MAX_LENGTH = 80
+
+cipher1 = "entehbitgqiesfkgzfgktlxwktubektbwktqerrffnubitnbekttnbtkbitkeerfgfaeswbtvtnbstrfnwggxaaegtubetnbtkfukfzwnvkeeruebitzfgktqtwctufbbitueekydfnebitkgqiesfkfnuqenuxqbtuhkerytnqibeytnqixnbwsitifuyttnwnbkeuxqtubefssbitdexnvsfuwtgfnuvtnbstrtnwnbitkeeruebswnqesnztnbbikexvibitekutfsqexnbstggbwrtguebwhitbeejfgtkwexgcwtzehbitatkhekrfnqtwbrxgbifctaxbiwrbetplxwgwbtbekbxktqerrfhekitzfgqengqwexgbifbitzfgnebfatkhtqbbdatehrfnsdytfxbd"
+
+cipher2 = "qtseoktfgacuetdukcduumrpulwtdedqmeykwttdxdiddyqtbfaymkyfacbueoxocehucuazgtleovucxocucaytidpoqfcdvdeqeqykftanekmdumrpupmfcamaueyhekcetgofulwtdedqmeykwtceuamfcamauetdxdidtocetgtocydurtdqidditohctkuhanekmdumrpupumtfkrpgktokteuemcelqcbrtdxdidluceoeetedoyofoxgacufzmpzsuesztcdtdqidofocekcetabeecotcnytugkiqkfgoctybrvdgnetptduumulukeofkycrtdqidofamuklhcaytulwttdxdidetuckpfubphimhdqidmeoftoqsulwtdedqmeykwttdxdidqtsaotfvxoetukruqeqfoetotcekeqlxiqhuceoeelrpulwtqftvneekcgdqidtkfgagmrdqidluoeelwtceoeqtyabeecytetdutogttdxdidofocetxofuvdytegdqidceoelufqgttdxdidukrucetekyctrprktyvdulwttotgdqidxmvdceduumukxoekmdumbhtcuyxhdqidofcxoahunaycetflofdtdqidfzceceguuezqwtgecfcuqfhuykaehtftydyuzqxocaidxptklufiyhvuaycnytezagykicyvorykicceqfcvtyotxgdqidulihxttgdqidkpueuhtdxdidgacursdvruouyhettkyhhurhdqideyyxqfcbayxoxmvdceduumulbhaqwtytulwtdedqmeykwtkttdxdidtkfzkqqscetuirmxybofocetstybbrqfxudqcetgtouhbdoedyrtdqidofcrdquifnydqcetyhefbutdxdidofvcdoxoaucgetdetkcecuxudqkiqkagycettkyutgdqidofcrydxvqfytcecaudfzwtyddqtdxdidofkrqeydyfcrdqidkiplzqsdqfezfooymelfoeqaxotycetfekmdumusthqfrktyfgmpmhdqidqtratohqdndanamxretkaekdmeuffotdzqhrtocvtyqtratoctwftgcgdyucgcksudulwtdykdediqiveqtovdyqayvdoyzqxofadiofvdhuafxoaycgdqidpgtfqfcecimakmtaaeotcttdxdidceoeetluytktzeegdqidofcrdqhuvdpxfzceefcuavtyqtratogytyetudyvyeeqicvdyqayvdoyzqxodiofugqfhcnvuvbuqfxutoytneetdetdlxofqztfcgdqidofackuqfebfloyetzqcdkmtaaeiqftytduebufytyefzcecehffefqyhtdxdidofocetpyeoycqfhcetagbuuexuhucktysyyqayvdcffhgccegfdykrvdyqayvdoycffhgcdiofytftagmduciczrcuqducceofceuamuyhceuchiqfhuanceuamuyhagbutgdqidofocdacfhftoucfhoeqfhcetuhyfeqiqeqyhqztcvythqfcetgtkbyrktyqtratoctwftgeoidoyvyucoaanafoyetzqcdkmtaaeiqfttdxdidofkrfqcwhtmrdqidofkroutdxdidqebkcuunyfoqtdxdidsycehuszhueqpetgofceqfadneetucyhanqetdxdidofocetoybrofcxoaufeyfndyellrhixedqidofcrdqideactbxdukrtdxdiduycdacpqrbtgkqihgcqetdxdidpgtfetekbhtyytoftfequcqsamnwauucoyfkycancehutohgdqidofvrucoaanxedqidfzceayeonvmcanfvyxqfhgdqidyttvuvhdofkmtcqtseokufcncetfofocetucvdyqayvdoyfgmpmhdqidzfqiiqyuchzatuayofmaueanekmdumrprhdqmeulwtqfhgdqidofocetoqayedfqrahtanqenabraybuhixatyflgcybteduumlsduumrktyyhofamwtsfqfxutoytnffeotofvocdcmhuulwtcecfcffhgcofcqvyvdeqidxuefamhyokcuqtmkceqfhgdqidqtsaotxvtyfuwftgyqaykiphotfzcgdqidqtsaotxvtysyeqhycetapqqyelwtlaotxvtyqeyhetfivchuykwtayytnfqfeadytfdtdqidmatocquexfqfeadyuficofqtsaotpwpgoeqzueoyettyetycelwttdxdidqtsaotxvtyqtriyueompiqaqfqoyetgedyyutdxdidqtsaotxvtyirmxybykxahizeecofvoxwfuyqytqaqfqfcetgtkbyrktysbqiiqofhncunamxofgtanxshiratctyetfivchuykwtqfcyfezsuqybrsqocukdyufloflucotyqdicoyduumagupdyekmdumyhaqaqfqxotyekmdumyhoahuototeqgtanqevdyocukdyufloflucgdqicxthuxshirataagudevqftdxdidofkcduumrhdqidtdxdicxtqfhbqfezwtoecehuditcqztgdqidvolshuanneetafbucngnkqqfhuanceuamuyhqkmcrpykwtayytnfqfeadycf"
+
+
+
+################################################################################
+
+class Cryptext():
+    def __init__(self, string=""):
+        self.chars = [Char(c) for c in string]
+
+    def __str__(self):
+        chars = [str(c) for c in self.chars]
+        return "".join(chars)
+
+    def __repr__(self):
+        return "Cryptext('{}')".format(str(self))
+
+    def shift(self, dist, keylen=1, keypos=0):
+        chars = self.chars[:]
+        for i in range(0, len(chars), keylen):
+            idx = i + keypos
+            if idx < len(chars):
+                chars[idx] = chars[idx].shift_left(dist)
+        cr = Cryptext()
+        cr.chars = chars
+        return cr
+
+    def swap(self, l1, l2):
+        def newchar(c):
+            c = str(c)
+            if c == l1:
+                return l2
+            elif c == l2:
+                return l1
+            else:
+                return c
+
+        newstring = "".join(newchar(c) for c in self.chars)
+        return Cryptext(newstring)
+
+    def map(self, fun):
+        cr = Cryptext(str(self))
+        cr.chars = [fun(c) for c in cr.chars]
+        return cr
+
+
+
+class Char():
+    def __init__(self, letter=None, num=None):
+        if letter:
+            letter = letter.lower()
+            self.num = ord(letter) - 96
+        elif num:
+            self.num = num
+        else:
+            assert(False)
+
+    # def __eq__(self, o):
+        # return self.num == o.num
+    # def __hash__(self):
+        # return self.num
+
+    def __repr__(self):
+        return "Char({c}, {n})".format(c=str(self), n=self.num) 
+    def __str__(self):
+        return str(chr(96 + self.num))
+
+    def shift_left(self, dist):
+        num = (self.num + 26 - dist) % 26
+        if num == 0:
+            num = 26
+        return Char(num=num)
+
+    # def shift(self, dist, keylen=1, keypos=0):
+
+# def rotations(cr, keylen):
+    # def top3(lcs): # [(letter, count)]
+        # lcs.sort(key=lambda lc: -lc[1])
+        # return lcs[:3]
+
+    # candidates = []
+    # for p in range(keylen):j
+        # for i in range(1,27):
+            # cc = cr.shift(i, keylen, p)
+            # print(str(cc)[:80])
+            # freqs = cc._frequencies()
+            # mosts = top3(freqs)
+            # topchars = "".join(l for l,_ in mosts)
+            # if "a" in topchars and "e" in topchars and "t" in topchars:
+                # candidates.append(cc)
+
+    # return candidates
+
+def freqlengths(cr, maxkeylen):
+    for i in range(1, maxkeylen): 
+        print(i)
+        cc = Cryptext(str(cr)[::i])
+        print(cc)
+        cc.print_frequencies()
+        print()
+
+
+class Freqeuncies():
+    def __init__(self, string):
+        self.counts = Counter(string) # {letter: count}
+        self.by_count = sorted(self.counts.items(), key=lambda lc: -lc[1])
+
+    def top(self, n):
+        top_lcs = self.by_count[:n]
+        topn = [l for l,c in top_lcs]
+        return topn
+
+    def show(self):
+        most = self.by_count[0][1]
+        scale = MAX_LENGTH / most
+        for l in ALPHAS:
+            n = self.counts[l]
+            print("{l}{n:4}:{stars}".format(l=l, n=n, stars="*" * int(scale * n)))
+
+def is_candidate(string):
+    freq = Freqeuncies(string)
+    top5 = freq.top(4)
+    return "e" in top5 and ("a" in top5 or "t" in top5)
+
+
+def ceasearcycle(string):
+    cr = Cryptext(string)
+    candidates = []
+    for i in range(1, 27):
+        s = str(cr.shift(i))
+        if is_candidate(s):
+            candidates.append((i,s))
+    return candidates
+
+
+ShiftStat = namedtuple("ShiftStat", ["shift", "keylen", "pos"])
+def vignerecycle(string):
+    candidates = []
+
+    for keylen in range(1, 12): # round(len(string)/5)):
+        # print(keylen)
+        for pos in range(0, keylen):
+            for shift in range(1, 27):
+                ss = ShiftStat(shift, keylen, pos)
+                # print(ss)
+                selectstring = string[pos::keylen]
+                cr = Cryptext(selectstring)
+                shiftstring = str(cr.shift(shift))
+                if is_candidate(shiftstring):
+                    candidates.append(ss)
+                    # print("     is candidate")
+
+    return candidates
+
+def group_vignere(string):
+    candidates = vignerecycle(string)
+    keylens = defaultdict(list)
+    for ss in candidates:
+        keylens[ss.keylen].append(ss)
+
+    narrow_candidates = {kl:sss for kl,sss in keylens.items() if num_pos(sss) >= kl - 1}
+    return narrow_candidates
+
+def num_pos(sss):
+    uniqe_pos = set(ss.pos for ss in sss)
+    return len(uniqe_pos)
+
+
+
